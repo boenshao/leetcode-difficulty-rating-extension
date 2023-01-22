e26969df3bf3455beb36bec46473eada6164406d
boenshao <boenshao@gmail.com>
2023-01-22 22:58:22 +0800

    fix: remember options between reload

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({showNA: true});
+chrome.runtime.onInstalled.addListener(async () => {
+  chrome.storage.local.set({
+    // set from saved value, default to true if not saved yet
+    showNA: (await chrome.storage.local.get({showNA: true})).showNA,
+  });
 });

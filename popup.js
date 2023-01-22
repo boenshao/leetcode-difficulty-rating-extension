4956ecd2609f1189a14aae128728249933948cfd
boenshao <boenshao@gmail.com>
2023-01-23 00:54:39 +0800

    refactor: reorg 'then' chain to a cleaner await

--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
-await chrome.storage.local.get("showNA").then((res) => {
-  document.getElementById("showNA").checked = res.showNA;
-});
-
-document.getElementById("showNA").addEventListener("change", async (e) => {
+let checkBoxShowNA = document.getElementById("showNA");
+checkBoxShowNA.checked = (await chrome.storage.local.get("showNA")).showNA;
+checkBoxShowNA.addEventListener("change", async (e) => {
   await chrome.storage.local.set({showNA: e.target.checked});
 });

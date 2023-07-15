89a65040340178a97a22953307e2fc1830210e10
boenshao <boenshao@gmail.com>
2023-07-15 13:03:09 +0800

    fix: select updated styling for new 'leetcode.com/problems/*/'

--- src/content.js
+++ src/content.js
@@ -73,7 +73,7 @@ const update = async () => {
   // new leetcode.com/problems/*/
   title = document.querySelector("div > span.text-lg.text-label-1.font-medium");
   difficulty = document.querySelector(
-    "div > div.text-xs.font-medium.capitalize"
+    "div > div.text-sm.font-medium.capitalize"
   );
   replace(ratings, title, difficulty, showNA);
 
